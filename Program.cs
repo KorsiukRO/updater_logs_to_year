deada75a8010a8e9e86d243cd82f1d8089e1b615
KorsiukRO <116555976+KorsiukRO@users.noreply.github.com>
2023-11-29 17:39:09 +0200

    2.0.0

--- Program.cs
+++ Program.cs
@@ -1,45 +1,46 @@
 ﻿using System.Xml;
 using Cocona;
 
-CoconaApp.Run(async (string days) =>
-    await updater_logs_to_year(days)
-);
-
-async Task updater_logs_to_year(string days)
+CoconaApp.Run((string days) =>
 {
     try
     {
-        var filePath = "C:/prosafe/scheduler/tasks.config";
-
-        if (!File.Exists(filePath)) //перевірка наявності файлу конфігурації
-        {
-            throw new InvalidOperationException($"\nERROR: {filePath} not found!");
-        }
-
-        // Завантаження XML-документа
-        XmlDocument xmlDoc = new XmlDocument();
-        xmlDoc.Load(filePath);
-
-        // Знайдення елементу <maxArchiveFiles>
-        XmlNode maxArchiveFilesNode = xmlDoc.SelectSingleNode("//maxArchiveFiles");
-
-        if (maxArchiveFilesNode != null)
-        {
-            // Зміна значення елементу <maxArchiveFiles>
-            maxArchiveFilesNode.InnerText = days;
-
-            // Збереження змін у файл
-            xmlDoc.Save(filePath);
-            Console.WriteLine($"The value of <maxArchiveFiles> was successfully changed to {days}.");
-        }
-        else
-        {
-            throw new InvalidOperationException("\n<maxArchiveFiles> not found!");
-        }
+        UpdateLogsToYear(days);
     }
     catch (Exception e)
     {
-        Console.WriteLine($"Error migration config file: {e.Message}");
+        Console.WriteLine(e);
+        throw;
     }
+});
+return;
+
+void UpdateLogsToYear(string days)
+{
+    const string filePath = "C:/prosafe/scheduler/tasks.config";
+
+    if (!File.Exists(filePath)) //перевірка наявності файлу конфігурації
+    {
+        Console.WriteLine($"\nERROR: {filePath} not found!");
+        return;
+    }
+
+    // Завантаження XML-документа
+    var xmlDoc = new XmlDocument();
+    xmlDoc.Load(filePath);
+
+    // Знайдення елементу <maxArchiveFiles>
+    var maxArchiveFilesNode = xmlDoc.SelectSingleNode("//maxArchiveFiles");
+    if (maxArchiveFilesNode is null)
+    {
+        Console.WriteLine("\n<maxArchiveFiles> not found!");
+        return;
+    }
+
+    // Зміна значення елементу <maxArchiveFiles>
+    maxArchiveFilesNode.InnerText = days;
+
+    // Збереження змін у файл
+    xmlDoc.Save(filePath);
+    Console.WriteLine($"The value of <maxArchiveFiles> was successfully changed to {days}.");
 }
-     
